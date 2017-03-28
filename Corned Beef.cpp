a13caa863e7b3a4f1bfa29e4f42298c3857b8f77
jodo993 <josephdo993@yahoo.com>
2017-03-27 23:35:05 -0700

    Korned Beaf Complete

--- Corned Beef.cpp
+++ Corned Beef.cpp
@@ -1,11 +1,25 @@
 // Corned Beef.cpp : Defines the entry point for the console application.
 //
 
+#include <map>
+#include <vector>
+#include <string>
 #include "stdafx.h"
-
+#include "cornedbeef.h"
 
 int main()
 {
+	std::map<std::string, std::string> myMap;		// Declare a map
+	
+	myMap[0] = "Tom", "one";
+	myMap[1] = "Tim", "two";
+	myMap[2] = "Tam", "twa";
+	myMap[3] = "Tum", "twe";
+
+	myMap.Contain("Tum");
+	myMap.SetWithKey("Tom", "one");
+	myMap.clear();
+
     return 0;
 }
 
