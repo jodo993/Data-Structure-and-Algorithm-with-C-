e14e4ebd05e3929750edb46553ebc3cc07ee011c
jodo993 <josephdo993@yahoo.com>
2017-05-08 19:40:45 -0700

    HufflePuffle 1.1

--- Header.h
+++ Header.h
@@ -1,13 +1,36 @@
 #pragma once
+#include <string>
+#include <fstream>
+#include <sstream>
 
 class Huffamn
 {
 private:
+	std::string name;
+
 public:
 	// give a file name, return the new number of bits
 	// total bits is the sum of all frequency * bitsize entries
 
-	int ProcessFile(std::string filename);
+	int ProcessFile(std::string filename) {
+		name = filename;
+		std::ifstream _infile;
+		_infile.open("filename");
+		std::string _stuff;
+
+		while (!_infile.eof()) {
+			_infile >> _stuff;
+		}
+	}
+
+	int buildNode(char _data, int _nodeWeight) {
+		HuffNode* temp = new HuffNode;
+		temp->_data = _data;
+		temp->_nodeWeight = _nodeWeight;
+		temp->_right = NULL;
+		temp->_left = NULL;
+		return temp;
+	}
 
 private:
 	unordered_map<char, Huffnode*> _table;
@@ -19,6 +42,12 @@ private:
 		char _data;
 		int _nodeWeight;
 		std::string _finalCode;
+		HuffNode();
 	};
+
+	~HuffNode() {
+		delete left;
+		delete right;
+	}
 
 };
